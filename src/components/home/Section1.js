c7f05fa304c0f8d1612a456db7f5e52c99af6f93
Chetan Yadav <imchetanyadav@gmail.com>
2018-12-20 21:15:37 +0530

    Removed unnecessary files

--- src/components/home/Section1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Section1 = () => (
-  <div>
-    <p>Section 1</p>
-  </div>
-)
-
-export default Section1
--- src/components/home/Section2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Section2 = () => (
-  <div>
-    <p>Section 2</p>
-  </div>
-)
-
-export default Section2
