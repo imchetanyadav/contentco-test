8b741f030b72dacf2e15f76a72d0454d84d8e391
Chetan Yadav <imchetanyadav@gmail.com>
2018-12-20 22:07:10 +0530

    Post page content and UI updated

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,14 +6,31 @@ const Post = ({ data }) => {
   const post = data.allPosts.edges[0].node;
 
   return (
-    <div>
+    <div style={{display: 'flex', justifyContent: 'center'}}>
       <Helmet
         title={post.title}
         meta={[
           { name: 'description', content:  post.excerpt},
         ]}
       />
-      {post.title}
+      <div style={{maxWidth: '800px'}}>
+        <img src={post.featured_image} style={{width: '100%'}} /> 
+        <div style={{display: 'flex'}}>
+          <div style={{marginTop: '6rem', color: 'grey', lineHeight: '1.5rem', minWidth: '150px', textAlign: 'right'}}>
+            {new Date(post.date_gmt).toDateString()}<br/>
+            {post.category}
+          </div>
+          <div style={{flexGrow: 1, paddingLeft: '1rem'}}>
+            <h1>{post.title}</h1>
+            <div 
+              className="dangerous-html"
+              dangerouslySetInnerHTML={{
+                __html: post.content
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
